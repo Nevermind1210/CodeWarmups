9170475b242e193aa6e1ed12b046698e160467d7
NegativeVibes <xavier_rate@outlook.com>
2020-08-08 10:41:17 +1000

    This is now the fully functioning Othello board, it took a bit to figure out sorta makes sense...

--- CodeWarmups13/Source.cpp
+++ CodeWarmups13/Source.cpp
@@ -2,28 +2,77 @@
 #include <vector>
 
 typedef std::vector<std::vector<char>> OthelloBoardState;
+
+bool CheckDirection(char player, int xdir, int ydir, int x, int y, const OthelloBoardState& grid)
+{
+    int xi = (x + xdir);
+    int yi = (y + ydir);
+    bool foundOtherPlayer = true;
+
+    while (xi >= 0 && xi < 8 && yi < 8 && grid[yi][xi] != player && grid[yi][xi] != ' ')
+    {
+        xi += xdir;
+        yi += ydir;
+        foundOtherPlayer = false;
+    }
+
+    if (xi >= 0 && xi < 8 && yi >= 0 && yi < 8 && grid[yi][xi] == player && !foundOtherPlayer)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
 bool IsValidMove(char player, int x, int y, const OthelloBoardState& grid)
 {
     // It is assumed that 'grid' will have exactily 8x8 rows/cols
     // TODO: calculate if a given coordinate is a valid move.
     //  the given coordinate must adhere to the rules of othello for 
     //  determining if the move is valid
-    if (x + 1 != player ||
-        x - 1 != player ||
-        x, y + 1 != player ||
-        x, y - 1 != player)
+   
+      // Look left
+    if (CheckDirection(player, -1, 0, x, y, grid))
     {
-        if (x + 1, y != player)
-        {
-            x + 1, y;
-            while (x + 1, y == player)
-                return true;
-        else
-
-        }
+        return true;
     }
-
-
+    // Look right
+    if (CheckDirection(player, 1, 0, x, y, grid))
+    {
+        return true;
+    }
+    // Look up
+    if (CheckDirection(player, 0, -1, x, y, grid))
+    {
+        return true;
+    }
+    // Look down
+    if (CheckDirection(player, 0, 1, x, y, grid))
+    {
+        return true;
+    }
+    // Look top left
+    if (CheckDirection(player, -1, -1, x, y, grid))
+    {
+        return true;
+    }
+    // Look top right
+    if (CheckDirection(player, 1, -1, x, y, grid))
+    {
+        return true;
+    }
+    // Look bottom left
+    if (CheckDirection(player, 1, 1, x, y, grid))
+    {
+        return true;
+    }
+    // Look bottom right
+    if (CheckDirection(player, -1, 1, x, y, grid))
+    {
+        return true;
+    }
+    return false;
 }
 OthelloBoardState MakeMove(char player, int x, int y, OthelloBoardState grid)
 {
@@ -52,15 +101,15 @@ int main(int argc, char** argv)
         std::cout << (result == expected ? "\033[32m [PASS]" : "\033[31m [FAIL]") << "\033[0m" << std::endl;
     };
     ShouldBe(IsValidMove('B', 5, 4, state1), true); // true
-    //ShouldBe(IsValidMove('B', 2, 4, state1), false); // false
-    //ShouldBe(IsValidMove('B', 3, 2, state1), true); // true
-    //ShouldBe(IsValidMove('B', 3, 5, state1), false); // false
-    //ShouldBe(IsValidMove('B', 5, 5, state1), false); // false
-    //ShouldBe(IsValidMove('W', 5, 4, state1), false); // false
-    //ShouldBe(IsValidMove('W', 2, 4, state1), true); // true
-    //ShouldBe(IsValidMove('W', 3, 2, state1), false); // false
-    //ShouldBe(IsValidMove('W', 3, 5, state1), true); // true
-    //ShouldBe(IsValidMove('W', 5, 5, state1), false); // false
+    ShouldBe(IsValidMove('B', 2, 4, state1), false); // false
+    ShouldBe(IsValidMove('B', 3, 2, state1), true); // true
+    ShouldBe(IsValidMove('B', 3, 5, state1), false); // false
+    ShouldBe(IsValidMove('B', 5, 5, state1), false); // false
+    ShouldBe(IsValidMove('W', 5, 4, state1), false); // false
+    ShouldBe(IsValidMove('W', 2, 4, state1), true); // true
+    ShouldBe(IsValidMove('W', 3, 2, state1), false); // false
+    ShouldBe(IsValidMove('W', 3, 5, state1), true); // true
+    ShouldBe(IsValidMove('W', 5, 5, state1), false); // false
     OthelloBoardState state2 =
     {//   0    1    2    3    4    5    6    7
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, // 0
@@ -72,15 +121,15 @@ int main(int argc, char** argv)
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, // 6
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}  // 7
     };
-    //ShouldBe(IsValidMove('B', 2, 5, state2), true); // true
-    //ShouldBe(IsValidMove('B', 3, 5, state2), false); // false
-    //ShouldBe(IsValidMove('B', 4, 5, state2), true); // true
-    //ShouldBe(IsValidMove('B', 3, 1, state2), false); // false
-    //ShouldBe(IsValidMove('B', 4, 2, state2), true); // true
-    //ShouldBe(IsValidMove('B', 4, 1, state2), true); // true
+    ShouldBe(IsValidMove('B', 2, 5, state2), true); // true
+    ShouldBe(IsValidMove('B', 3, 5, state2), false); // false
+    ShouldBe(IsValidMove('B', 4, 5, state2), true); // true
+    ShouldBe(IsValidMove('B', 3, 1, state2), false); // false
+    ShouldBe(IsValidMove('B', 4, 2, state2), true); // true
+    ShouldBe(IsValidMove('B', 4, 1, state2), true); // true
 
-    //ShouldBe(IsValidMove('W', 2, 5, state2), false); // false
-    //ShouldBe(IsValidMove('W', 2, 0, state2), true); // true
-    //ShouldBe(IsValidMove('W', 3, 1, state2), false); // false
+    ShouldBe(IsValidMove('W', 2, 5, state2), false); // false
+    ShouldBe(IsValidMove('W', 2, 0, state2), true); // true
+    ShouldBe(IsValidMove('W', 3, 1, state2), false); // false
     return 0;
 }
